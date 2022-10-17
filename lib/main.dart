32f7bbf835f7129ef94a67b4519f52f09c8380e4
abhi9827 <abhilamixane1@gmail.com>
2022-10-17 20:49:27 +0545

    zsdf

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'Homepage.dart';
+import 'Signup.dart';
+import 'Start.dart';
+import 'login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.orange),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      routes: <String, WidgetBuilder>{
+        "Login": (BuildContext context) => Login(),
+        "SignUp": (BuildContext context) => SignUp(),
+        "start": (BuildContext context) => Start(),
+      },
+    );
+  }
+}
